dbd64b9527ba8b8d549cf55b09bf22cab6131345
Ryan Hu <ryhu@student.unimelb.edu.au>
2024-10-21 12:45:13 -0400

    Completed TODO: LogoutPresenter: - // TODO: assign to the three instance variables. - // TODO: have prepareSuccessView update the LoggedInState - // TODO: have prepareSuccessView update the LoginState
    LogoutController:
    - // TODO: Save the interactor in the instance variable.
    // TODO: run the use case interactor for the logout use case

--- src/main/java/interface_adapter/logout/LogoutPresenter.java
+++ src/main/java/interface_adapter/logout/LogoutPresenter.java
@@ -1,6 +1,7 @@
 package interface_adapter.logout;
 
 import interface_adapter.ViewManagerModel;
+import interface_adapter.change_password.LoggedInState;
 import interface_adapter.change_password.LoggedInViewModel;
 import interface_adapter.login.LoginViewModel;
 import use_case.logout.LogoutOutputBoundary;
@@ -11,14 +12,17 @@ import use_case.logout.LogoutOutputData;
  */
 public class LogoutPresenter implements LogoutOutputBoundary {
 
-    private LoggedInViewModel loggedInViewModel;
-    private ViewManagerModel viewManagerModel;
-    private LoginViewModel loginViewModel;
+    private final LoggedInViewModel loggedInViewModel;
+    private final ViewManagerModel viewManagerModel;
+    private final LoginViewModel loginViewModel;
 
     public LogoutPresenter(ViewManagerModel viewManagerModel,
                           LoggedInViewModel loggedInViewModel,
                            LoginViewModel loginViewModel) {
-        // TODO: assign to the three instance variables.
+
+        this.loggedInViewModel = loggedInViewModel;
+        this.viewManagerModel = viewManagerModel;
+        this.loginViewModel = loginViewModel;
     }
 
     @Override
@@ -29,17 +33,23 @@ public class LogoutPresenter implements LogoutOutputBoundary {
         // We also need to set the username in the LoggedInState to
         // the empty string.
 
-        // TODO: have prepareSuccessView update the LoggedInState
         // 1. get the LoggedInState out of the appropriate View Model,
+        var loggedInState = loggedInViewModel.getState();
         // 2. set the username in the state to the empty string
+        loggedInState.setUsername("");
         // 3. set the state in the LoggedInViewModel to the updated state
+        loggedInViewModel.setState(loggedInState);
         // 4. firePropertyChanged so that the View that is listening is updated.
+        loggedInViewModel.firePropertyChanged();
 
-        // TODO: have prepareSuccessView update the LoginState
         // 5. get the LoginState out of the appropriate View Model,
+        var loginState = loginViewModel.getState();
         // 6. set the username and password in the state to the empty string
+        loginState.setUsername("");
         // 7. set the state in the LoginViewModel to the updated state
+        loginViewModel.setState(loginState);
         // 8. firePropertyChanged so that the View that is listening is updated.
+        loginViewModel.firePropertyChanged();
 
         // This code tells the View Manager to switch to the LoginView.
         this.viewManagerModel.setState(loginViewModel.getViewName());
